77ae60ad2153ff2ea8912e0f4efd28fca6a69ab2
raimax <4miner44@gmail.com>
2023-04-01 19:25:49 +0300

    final

--- sandbox/app.py
+++ sandbox/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from Classifier import Classifier
 from Algorithm import Algorithm
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 naivePredictor = Classifier(Algorithm.NaiveBayes)
 naivePredictor.initialize()
@@ -12,21 +14,21 @@ logisticPredictor = Classifier(Algorithm.LogisticRegression)
 logisticPredictor.initialize()
 
 
-@app.route('/naive-bayes/predict/<message>', methods=['GET'])
+@app.route('/api/naive-bayes/predict/<message>', methods=['GET'])
 def naivePredict(message):
     return jsonify(isSpam=naivePredictor.predict(message))
 
 
-@app.route('/naive-bayes/model', methods=['GET'])
+@app.route('/api/naive-bayes/model', methods=['GET'])
 def getNaiveModel():
     return jsonify(accuracy=naivePredictor.accuracy)
 
 
-@app.route('/logistic-regression/predict/<message>', methods=['GET'])
+@app.route('/api/logistic-regression/predict/<message>', methods=['GET'])
 def logisticPredict(message):
     return jsonify(isSpam=logisticPredictor.predict(message))
 
 
-@app.route('/logistic-regression/model', methods=['GET'])
+@app.route('/api/logistic-regression/model', methods=['GET'])
 def getLogisticModel():
     return jsonify(accuracy=logisticPredictor.accuracy)
